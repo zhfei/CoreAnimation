c169021f4bc3f18775ef3d816be8eb88c92b6f92
zhoufei <1174977961@qq.com>
2019-07-16 10:56:55 +0800

    feature:完善拨打电话动画

--- CoreAnimation/Animations/电话拨打中动画/PhoneCallViewController.m
+++ CoreAnimation/Animations/电话拨打中动画/PhoneCallViewController.m
@@ -10,8 +10,10 @@
 #import "CallingView.h"
 
 
-@interface PhoneCallViewController ()
 
+@interface PhoneCallViewController ()
+@property (nonatomic, strong) CallingView *leftCallView;
+@property (nonatomic, strong) CallingView *rightCallView;
 @end
 
 @implementation PhoneCallViewController
@@ -20,25 +22,62 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     
-    CallingView *callView = [[CallingView alloc] initWithFrame:CGRectMake(20, 100, 300, 300)];
     
-    callView.layer.cornerRadius = 5;
-    callView.layer.borderWidth = 1;
-    callView.layer.borderColor = [UIColor lightGrayColor].CGColor;
-    callView.layer.masksToBounds = YES;
-    
-    
-    [self.view addSubview:callView];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    [self setupUI];
 }
-*/
+
+- (void)setupUI {
+
+    [self.view addSubview:self.leftCallView];
+    [self.view addSubview:self.rightCallView];
+    
+    UILabel *label = [UILabel new];
+    label.text = @"拨打电话";
+    label.textColor = [UIColor blackColor];
+    label.textAlignment = NSTextAlignmentCenter;
+    label.font = [UIFont systemFontOfSize:16];
+    label.frame = CGRectMake(CGRectGetMaxX(self.leftCallView.frame), CGRectGetMinY(self.leftCallView.frame), CGRectGetMinX(self.rightCallView.frame)-CGRectGetMaxX(self.leftCallView.frame),40);
+    [self.view addSubview:label];
+}
+
+
+
+- (CallingView *)leftCallView {
+    if (!_leftCallView) {
+        CGFloat widthS = [UIScreen mainScreen].bounds.size.width;
+        CGFloat heightS = [UIScreen mainScreen].bounds.size.height;
+        
+        CallingView *callView = [[CallingView alloc] initWithFrame:CGRectMake(widthS/2-80, heightS/2, 40, 40)];
+        callView.layer.cornerRadius = 5;
+        callView.layer.borderWidth = 1;
+        callView.layer.borderColor = [UIColor lightGrayColor].CGColor;
+        callView.layer.masksToBounds = YES;
+        _leftCallView = callView;
+
+    }
+    return _leftCallView;
+}
+
+- (CallingView *)rightCallView {
+    if (!_rightCallView) {
+        CGFloat widthS = [UIScreen mainScreen].bounds.size.width;
+        CGFloat heightS = [UIScreen mainScreen].bounds.size.height;
+        
+        CallingView *callView = [[CallingView alloc] initWithFrame:CGRectMake(widthS/2+80, heightS/2, 40, 40)];
+        callView.layer.cornerRadius = 5;
+        callView.layer.borderWidth = 1;
+        callView.layer.borderColor = [UIColor lightGrayColor].CGColor;
+        callView.layer.masksToBounds = YES;
+        _rightCallView = callView;
+        
+    }
+    return _rightCallView;
+
+}
+
 
 @end
--- CoreAnimation/Animations/电话拨打中动画/View/CallingView.m
+++ CoreAnimation/Animations/电话拨打中动画/View/CallingView.m
@@ -8,8 +8,9 @@
 
 #import "CallingView.h"
 
-const CGFloat callingViewHeight = 40;
-const CGFloat callingViewWidth = 40;
+const CGFloat circleWidthAndHeight = 4;
+const CGFloat circleMargin = 6;
+const CGFloat animationTime = 1;
 
 
 @implementation CallingView
@@ -29,54 +30,27 @@ const CGFloat callingViewWidth = 40;
 }
 
 - (void)setupDownView {
-    
-    // 1.创建一个复制图层对象，设置复制层的属性
+    // 1.创建一个重复图层
     CAReplicatorLayer *replicatorLayer = [CAReplicatorLayer layer];
-    
-    // 1.1.设置复制图层中子层总数：这里包含原始层
     replicatorLayer.instanceCount = 3;
-    // 1.2.设置复制子层偏移量，不包含原始层，这里是相对于原始层的x轴的偏移量
-    replicatorLayer.instanceTransform = CATransform3DMakeTranslation(60, 0, 0);
-    // 1.3.设置复制层的动画延迟事件
-    replicatorLayer.instanceDelay = 0.2;
-    // 1.4.设置复制层的背景色，如果原始层设置了背景色，这里设置就失去效果
-    replicatorLayer.instanceColor = [UIColor blueColor].CGColor;
-    // 1.5.设置复制层颜色的偏移量
-    replicatorLayer.instanceGreenOffset = -0.5;
+    replicatorLayer.instanceTransform = CATransform3DMakeTranslation(circleWidthAndHeight+circleMargin, 0, 0);
+    replicatorLayer.instanceDelay = animationTime;
+    replicatorLayer.instanceColor = [UIColor colorWithRed:151/255.0 green:188/255.0 blue:255/255.0 alpha:1.0].CGColor;
     
-    // 2.创建一个图层对象  单条柱形 (原始层)
+    // 2.创建一个图层圆球
     CALayer *layer = [CALayer layer];
-    // 2.1.设置layer对象的位置
-    layer.position = CGPointMake(15, self.bounds.size.height);
-    // 2.2.设置layer对象的锚点
-    layer.anchorPoint = CGPointMake(0, 1);
-    // 2.3.设置layer对象的位置大小
-    layer.bounds = CGRectMake(0, 0, 30, 30);
-    // 2.5.设置layer对象的颜色
+    layer.position = CGPointMake(self.bounds.size.width/4, self.bounds.size.height/2);
+    layer.bounds = CGRectMake(0, 0, circleWidthAndHeight, circleWidthAndHeight);
     layer.backgroundColor = [UIColor whiteColor].CGColor;
+    layer.cornerRadius = circleWidthAndHeight/2;
+    layer.masksToBounds = YES;
     
+    // 4.创建动画并添加到layer层上
     CABasicAnimation *basicB = [CABasicAnimation animationWithKeyPath:@"opacity"];
     basicB.toValue = @(0);
     basicB.repeatCount = MAXFLOAT;
-    basicB.duration = 1;
+    basicB.duration = animationTime;
     basicB.autoreverses = YES;
-    
-//    // 3.创建一个基本动画
-//    CABasicAnimation *basicAnimation = [CABasicAnimation animation];
-//    // 3.1.设置动画的属性
-//    basicAnimation.keyPath = @"transform.scale.y";
-//    // 3.2.设置动画的属性值
-//    basicAnimation.toValue = @0.1;
-//    // 3.3.设置动画的重复次数
-//    basicAnimation.repeatCount = MAXFLOAT;
-//    // 3.4.设置动画的执行时间
-//    basicAnimation.duration = 0.5;
-//    // 3.5.设置动画反转
-//    basicAnimation.autoreverses = YES;
-    
-
-    
-    // 4.将动画添加到layer层上
     [layer addAnimation:basicB forKey:nil];
     
     // 5.将layer层添加到复制层上
